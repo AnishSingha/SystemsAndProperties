fa56894ce79a0d8223f825d241daef9329269520
AnishSingha <106175433+AnishSingha@users.noreply.github.com>
2023-12-21 19:46:13 +0530

    Created Obstacles and EDashState Script

---
+++ Assets/Scripts/AIEnemy/EDashState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EDashState : EnemyBaseState
+{
+    public override void EnterState(EnemyBehaviourManager enemyBaseState)
+    {
+
+    }
+
+    public override void UpdateState(EnemyBehaviourManager enemyBaseState)
+    {
+
+    }
+
+    public override void OnCollisionEnter(EnemyBehaviourManager enemyBaseState)
+    {
+
+    }
+}
